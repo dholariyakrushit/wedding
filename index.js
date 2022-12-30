91cf9bb7e8ead8cd146a4cfbc857f03fb35209b3
Krushit <krushit@pop-os.localdomain>
2022-12-30 17:07:19 +0530

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const hbs=require("hbs");
+const port=process.env.PORT || 7000
+
+
+const staticPath=path.join(__dirname,"./public");
+const templatesPath=path.join(__dirname,"./views")
+
+
+app.use(express.static(staticPath));
+
+app.set("views",templatesPath);
+
+// set the view engine
+app.set("view engine","hbs");
+// hbs.registerPartials(partialPath);
+
+app.get("/wedding",(req,res)=>{
+
+    res.render("index")
+    // res.sendFile(__dirname,"index.html")
+});
+
+
+app.listen(port,()=>{
+    console.log(`connect port ${port}`);
+})
