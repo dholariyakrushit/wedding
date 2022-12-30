dcff3259ca673b279f82c73d4a9c7d9cf031d1bf
Krushit <krushit@pop-os.localdomain>
2022-12-30 17:59:25 +0530

    route app

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const templatesPath=path.join(__dirname,"./views")
 
 
 app.use(express.static(staticPath));
+app.use(express.static(path.join(__dirname, "./public/script.js")));
+
+app.use(express.Router())
 
 app.set("views",templatesPath);
 
@@ -18,7 +21,7 @@ app.set("view engine","hbs");
 // hbs.registerPartials(partialPath);
 
 app.get("/wedding",(req,res)=>{
-
+    
     res.render("index")
     // res.sendFile(__dirname,"index.html")
 });
